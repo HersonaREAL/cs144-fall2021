25a19ed59b86595fe94d241af35677b0568aa007
cc <jycdeya@gmail.com>
2022-01-18 21:31:32 +0800

    feat(reassembler): init

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -12,15 +12,125 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
+StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {
+
+}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    if (index >= m_eofPos) {
+        fflush();
+        return;
+    }
+    
+    //TODO overlap
+    size_t rlen = data.size();
+    size_t ridx = index;
+    size_t offset = 0;
+    auto it = m_substrs.lower_bound(index);
+    if (it != m_substrs.end() && it != m_substrs.begin()) {
+        //cut tail
+        if (ridx + rlen > it->first) {
+            rlen = it->first - ridx;
+        }
+
+        //cut head
+        --it;
+        if (it->first + it->second.size() > ridx) {
+            ridx = it->first + it->second.size();
+            if (rlen < (ridx - index)) return;
+            rlen -= ridx - index;
+            offset += ridx - index;
+        }
+    } else if (it == m_substrs.begin() && it != m_substrs.end()) {
+        // data will insert to the map begin, cut tail of data if necessary 
+        rlen = ridx + rlen > it->first ? it->first - ridx : rlen;
+        if (ridx < m_pos) {
+            if (rlen < (m_pos - ridx)) return;
+            rlen -= m_pos - ridx;
+            offset += m_pos - ridx;
+            ridx = m_pos;
+        }
+    } else if (it == m_substrs.end() && !m_substrs.empty()) {
+        // data will insert to the map end, cut head of data if necessary
+        --it;
+        if (it->first + it->second.size() > ridx) {
+            ridx = it->first + it->second.size();
+            if (rlen < (ridx - index)) return;
+            rlen -= ridx - index;
+            offset += ridx - index;
+        }
+    } else if (m_substrs.empty()) {
+        // insert to empty map, cut head if necessary
+        if (ridx < m_pos) {
+            if (rlen < (m_pos - ridx)) return;
+            rlen -= m_pos - ridx;
+            offset += m_pos - ridx;
+            ridx = m_pos;
+        }
+    }
+
+    // can not store this chunk
+    if (m_pos != ridx && 
+            (unassembled_bytes() + _output.buffer_size() + rlen > _capacity)) {
+        fflush();
+        return;
+    }
+
+    // update substr
+    if (rlen > 0) {
+        string &&tmp = data.substr(offset, rlen);
+        m_substrSize += rlen;
+        m_substrs[ridx] = tmp;
+    }
+
+    // deal with eof
+    if (eof) {
+        m_eofPos = ridx + rlen;
+        fflush();
+    }
+
+    // write to bytestream
+    fflush();
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const {
+    return m_substrSize;
+}
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const { 
+    return m_substrs.empty();
+}
+
+void StreamReassembler::fflush() {
+    auto it = m_substrs.begin();
+    while (it != m_substrs.end()) {
+        if (it->first != m_pos) return;
+
+        uint64_t nextIdx = it->first + it->second.size();
+        const string& data = it->second;
+        size_t sz = data.size();
+        auto ret = _output.write(data);
+        m_substrSize -= ret;
+
+        // write part of data
+        if (ret == 0)
+            return;
+        if (ret != sz) {
+            string &&leftStr = data.substr(sz - ret);
+            m_pos = m_pos + ret;
+            m_substrs[m_pos] = leftStr;
+            m_substrs.erase(it);
+            return;
+        }
+
+        m_pos = nextIdx;
+        m_substrs.erase(it++);
+    }
+
+    if (m_pos == m_eofPos) {
+        _output.end_input();
+    }
+}
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -5,15 +5,22 @@
 
 #include <cstdint>
 #include <string>
+#include <map>
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
 class StreamReassembler {
   private:
     // Your code here -- add private members as necessary.
+    uint64_t m_pos = 0;   // cur position
+    uint64_t m_eofPos = -1; // eof pos
+    size_t m_substrSize = 0; //cur sub strs size
+    std::map<uint64_t, std::string> m_substrs{};
 
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
+  private:
+    void fflush();
 
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
